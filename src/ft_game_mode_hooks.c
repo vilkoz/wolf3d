a9590bfc4228af53f5e61c42ebd2fc2e0b2db23b
vilkoz <vilko.a@gmail.com>
2017-03-19 20:12:21 +0200

    trash

--- src/ft_game_mode_hooks.c
+++ src/ft_game_mode_hooks.c
@@ -6,7 +6,7 @@
 /*   By: vrybalko <vrybalko@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/28 16:12:45 by vrybalko          #+#    #+#             */
-/*   Updated: 2017/03/02 15:24:30 by vrybalko         ###   ########.fr       */
+/*   Updated: 2017/03/02 23:09:03 by vrybalko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,13 @@ void		game_key_press(int	key, t_e *e)
 	(key == 2) ? e->k.move_x = -1 : (void)e->k.gopa;
 	(key == 123) ? e->k.rot = 1 : (void)e->k.gopa;
 	(key == 124) ? e->k.rot = -1 : (void)e->k.gopa;
-	//(key == 115) ? e->k.move_y = -1 : (void)e->k.gopa;//LINUX
-	//(key == 119) ? e->k.move_y = 1 : (void)e->k.gopa;	//LINUX
-	//(key == 65361) ? e->k.rot = 1 : (void)e->k.gopa;	//LINUX
-	//(key == 65363) ? e->k.rot = -1 : (void)e->k.gopa;	//LINUX
-	//(key == 65307) ? exit(0) : (void)e->k.gopa;		//LINUX
-	//(key == 97) ? e->k.move_x = 1 : (void)e->k.gopa;	//LINUX
-	//(key == 100) ? e->k.move_x = -1 : (void)e->k.gopa;//LINUX
+	(key == 115) ? e->k.move_y = -1 : (void)e->k.gopa;//LINUX
+	(key == 119) ? e->k.move_y = 1 : (void)e->k.gopa;	//LINUX
+	(key == 65361) ? e->k.rot = 1 : (void)e->k.gopa;	//LINUX
+	(key == 65363) ? e->k.rot = -1 : (void)e->k.gopa;	//LINUX
+	(key == 65307) ? exit(0) : (void)e->k.gopa;		//LINUX
+	(key == 97) ? e->k.move_x = 1 : (void)e->k.gopa;	//LINUX
+	(key == 100) ? e->k.move_x = -1 : (void)e->k.gopa;//LINUX
 }
 
 void		game_key_release(int key, t_e *e)
@@ -41,17 +41,17 @@ void		game_key_release(int key, t_e *e)
 	(key == 49) ? ft_shoot(e) : (void)e->k.gopa;
 	(key == 14) ? ft_open_door(e) : (void)e->k.gopa;
 	(key == 46) ? e->k.map += 1 : 23;
-	//(key == 32) ? ft_open_door(e) : (void)e->k.gopa;	//LINUX
-	//(key == 119) ? e->k.move_y = 0 : (void)e->k.gopa;	//LINUX
-	//(key == 115) ? e->k.move_y = 0 : (void)e->k.gopa;	//LINUX
-	//(key == 65361) ? e->k.rot = 0 : (void)e->k.gopa;	//LINUX
-	//(key == 65363) ? e->k.rot = 0 : (void)e->k.gopa;	//LINUX
-	//if (key == 116 && e->k.tex == 1)					//LINUX
-	//	e->k.tex = 0;									//LINUX
-	//else if (key == 116 && e->k.tex == 0)				//LINUX
-	//	e->k.tex = 1;									//LINUX
-	//(key == 97) ? e->k.move_x = 0 : (void)e->k.gopa;	//LINUX
-	//(key == 100) ? e->k.move_x = 0 : (void)e->k.gopa;	//LINUX
+	(key == 32) ? ft_open_door(e) : (void)e->k.gopa;	//LINUX
+	(key == 119) ? e->k.move_y = 0 : (void)e->k.gopa;	//LINUX
+	(key == 115) ? e->k.move_y = 0 : (void)e->k.gopa;	//LINUX
+	(key == 65361) ? e->k.rot = 0 : (void)e->k.gopa;	//LINUX
+	(key == 65363) ? e->k.rot = 0 : (void)e->k.gopa;	//LINUX
+	if (key == 116 && e->k.tex == 1)					//LINUX
+		e->k.tex = 0;									//LINUX
+	else if (key == 116 && e->k.tex == 0)				//LINUX
+		e->k.tex = 1;									//LINUX
+	(key == 97) ? e->k.move_x = 0 : (void)e->k.gopa;	//LINUX
+	(key == 100) ? e->k.move_x = 0 : (void)e->k.gopa;	//LINUX
 }
 
 void		game_move_hook(t_e *e, int x, int y)
